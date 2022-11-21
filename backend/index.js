f68f28a8b5f4b883c18e468a2eabbea27c62202a
salt-dev <salt-dev@saltdevsair10.home>
2022-11-21 16:30:39 +0100

    changed get and post methods

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const uuidv4 = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 const dotenv = require("dotenv");
 dotenv.config();
@@ -23,31 +23,21 @@ const getUsers = async () => {
     }
 };
 
-const getUsersCards = async () => {
-    try {
-        const result = await DB.query('SELECT * FROM users cards');
-        return result.rows;
-    } catch (error) {
-        console.log(error);
-        console.log('Error fetching users pokemon cards.', error);
-        return [];
-    }
-};
-
-const createCardForSell = async (data) => {
+const createUser = async (data) => {
     try {
         const id = uuidv4();
-        const createdAt = new Date().getMilliseconds();
-        const { amount, description, operator } = data;
-
+        console.log(id);
+        const date = new Date().toString();
+        const createdAt = date.slice(0, 24);
+        const { name, email, password } = data;
         await DB.query(
-            `INSERT INTO "transactions" ("amount", "id", "description", "createdAt", "operator")  
-             VALUES ($1, $2, $3, $4, $5)`, [amount, id, description, createdAt, operator]);
+            `INSERT INTO "users" ("id", "name", "email", "password", "createdAt")  
+             VALUES ($1, $2, $3, $4, $5)`, [id, name, email, password, createdAt]);
 
     } catch (error) {
-        console.log('Error inserting transaction.', error);
+        console.log('Error inserting user.', error);
     }
-}
+};
 
 app.get('/users', async (req, res) => {
     const result = await getUsers();
@@ -57,7 +47,27 @@ app.get('/users', async (req, res) => {
     return res.json(result);
 });
 
+app.post('/users', async (req, res) => {
+    const data = req.body;
+    const { name, email, password } = data;
+    if (!name || !email || !password) {
+        return res.status(400).send({ error: 'Invalid input.' });
+    }
+    await createUser(data);
+    return res.json({ success: true });
+});
 
 app.listen(3001, () => {
     console.log('app listening on port 3001');
 });
+
+const getUserCards = async (userId) => {//still need to be fixed.
+    try {
+        const result = await DB.query(`SELECT * FROM cards WHERE cards.userId = $1`, [userId]);
+        return result.rows;
+    } catch (error) {
+        console.log(error);
+        console.log('Error fetching users pokemon cards.', error);
+        return [];
+    }
+};
